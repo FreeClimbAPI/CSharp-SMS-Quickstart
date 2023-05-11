9e0af0c8ef07e8bfa93d3ac84574b4d338e8991e
Ajevan M <amahadaya@vailsys.com>
2023-05-11 16:30:38 -0400

    Update freeclimb version

--- Controllers/FreeClimbController.cs
+++ Controllers/FreeClimbController.cs
@@ -1,33 +1,54 @@
-﻿using com.freeclimb;
-using com.freeclimb.percl;
-using com.freeclimb.webhooks.call;
-using System;
+﻿using System;
 using Microsoft.AspNetCore.Mvc;
-using com.freeclimb.api;
+using freeclimb.Api;
+using freeclimb.Model;
+using freeclimb.Enums;
+using System.Collections.Generic;
 
-namespace ReceiveMessage.Controllers {
-  [Route ("inboundSms")]
-  [ApiController]
-  public class FreeClimbController : ControllerBase {
+namespace ReceiveMessage.Controllers
+{
+    [Route("/voice")]
+    [ApiController]
+    public class FreeClimbController : ControllerBase
+    {
+        [HttpPost]
+        public string Post(CallStatus request)
+        {
+            PerclScript helloScript = new PerclScript(new List<PerclCommand>());
+            string to = getToNumber();
+            // Use as env variable rather than defining it in the code
+            string from = getFromNumber();
+            Console.WriteLine("to: " + to);
+            Console.WriteLine("from: " + from);
+            // Create a SMS Command
+            Sms messageToSend = new Sms(to, from, "Hello from the C# SDK!", "https://test.abc");
 
-    [HttpPost]
-    public ActionResult smsConnect (CallStatusCallback freeClimbRequest) {
-        string acctId = getAcctId ();
-        string apiKey = getApiKey ();
-        FreeClimbClient client = new FreeClimbClient (acctId, apiKey);
-        string to = freeClimbRequest.getFrom;
-        string from = "";
-        client.getMessagesRequester.create(from, to, "Hello from the C# SDK!");
-       
-        return Ok();
+            Console.WriteLine(messageToSend.ToJson());
+            // Add the command
+            helloScript.Commands.Add(messageToSend);
+
+            Console.WriteLine(helloScript.ToJson());
+
+            // Respond to FreeClimb with your script
+            return helloScript.ToJson();
+        }
+
+        private string getAcctId()
+        {
+            return System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
+        }
+
+        private string getApiKey()
+        {
+            return System.Environment.GetEnvironmentVariable("API_KEY");
+        }
+        private string getFromNumber()
+        {
+            return System.Environment.GetEnvironmentVariable("FROM_NUMBER");
+        }
+        private static string getToNumber()
+        {
+            return System.Environment.GetEnvironmentVariable("TO_NUMBER");
+        }
     }
-
-    private string getAcctId () {
-        return System.Environment.GetEnvironmentVariable("ACCOUNT_ID");
-    }
-
-    private string getApiKey () {
-        return System.Environment.GetEnvironmentVariable("API_KEY");
-    }
-  }
 }
