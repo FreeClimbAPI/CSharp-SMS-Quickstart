12c5f3a8bc2e6e7b9954c1a0fed5a7546f7ea21b
Bhargav Ganesh <bganesh@vailsys.com>
2024-04-25 10:35:27 -0500

    Added dockerfile, actions and other minor changes

--- Program.cs
+++ Program.cs
@@ -20,5 +20,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+Console.WriteLine("Welcome to FreeClimb!\n");
+Console.WriteLine("Your web server is listening at http://127.0.0.1:3000");
+Console.WriteLine("View an example PerCL JSON response to FreeClimb at http://127.0.0.1:3000/swagger/index.html\n");
+Console.WriteLine("Your NEXT STEP is to use NGROK to proxy HTTP traffic to this local web server.");
+Console.WriteLine("\t1. In NGROK, configure the dynamic url to proxy to http://127.0.0.1:3000");
+Console.WriteLine("\t2. Using the Dashboard or API, set your FreeClimb Application Voice Url to the dynamic endpoint NGROK generated\n");
+
 app.Run();
 
